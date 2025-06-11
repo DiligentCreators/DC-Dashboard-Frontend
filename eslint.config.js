d5608682a30131e93bdc6debbb02dbe777b2abdc
Shahrukh Akhtar <iam@shaz3e.com>
2025-06-11 20:07:38 +0500

    `Update ESLint configuration and workflow to use ESLint Fix and Prettier`

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,28 @@
-// eslint.config.js
 import js from "@eslint/js";
+import nuxt from "eslint-plugin-nuxt";
+import prettier from "eslint-config-prettier";
 
 export default [
-  js.configs.recommended,
   {
-    files: ["**/*.js", "**/*.ts"],
+    ignores: [".nuxt/**", "node_modules/**", "dist/**"],
+  },
+  js.configs.recommended,
+  nuxt.configs.recommended,
+  {
+    files: ["**/*.{js,ts,vue}"],
+    languageOptions: {
+      globals: {
+        useCookie: "readonly",
+        useFetch: "readonly",
+        useRequestHeaders: "readonly",
+        $fetch: "readonly",
+        defineStore: "readonly",
+      },
+    },
     rules: {
-      semi: ["error", "always"],
       quotes: ["error", "single"],
+      semi: ["error", "always"],
     },
   },
+  prettier, // ⬅️ Add this last to disable formatting conflicts
 ];
